b0ee445dfd3c75793a22f7ecf300848cfe32619f
Cameron Blackwood <cameronjwblackwood@gmail.com>
2020-09-22 08:59:19 +0100

    scripts for upper and lower case

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,24 @@ function App() {
   };
   return (
     <div className="App">
-      I want... Numbers only!
+      I want...
+      <ul>
+        <li>
+          Numbers <input type="checkbox" />
+        </li>
+        <li>
+          Lowercase <input type="checkbox" />
+        </li>
+        <li>
+          Uppercase <input type="checkbox" />{" "}
+        </li>
+        <li>
+          Specials <input type="checkbox" />
+        </li>
+        <li>plus... how many words?</li>
+      </ul>
       <input type="number" value={integer} onChange={handleIntegerChange} />
-      {integer ? randomInteger(integer) : ""}
+      {integer ? randomInteger(integer, 5) : ""}
     </div>
   );
 }
--- src/scripts/integer.jsx
+++ src/scripts/integer.jsx
@@ -1,24 +1,38 @@
 import React from "react";
 
-export default function randomInteger(length, numberOfWords) {
+export default function randomInteger(length, numberOfWords, includeWords) {
   const digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   const lowercase = "abcdefghijklmnopqrstuvwxyz".split("");
   const uppercase = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
-  const specials = "!@£$%^&*()+_{}";
+  const specials = "!@£$%^&*()+_{}".split("");
   const allChars = [digits, lowercase, uppercase, specials];
   let result = [];
-  if (numberOfWords > 0) {
-    let words = [];
-    for (let i = 0; i < numberOfWords; i++) {
-      
-      words.push("word");
-    }
+  if (includeWords) {
+    let words = fetch(
+      `https://random-word-api.herokuapp.com/word?number=${numberOfWords}`
+    )
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+      });
   }
   for (let i = 0; i < length; i++) {
     let randomElement = Math.floor(Math.floor(Math.random() * 10));
     result.push(digits[randomElement]);
   }
-  let integer = result.join("");
+  for (let i = 0; i < length; i++) {
+    let randomElement = Math.floor(Math.floor(Math.random() * 10));
+    result.push(lowercase[randomElement]);
+  }
+  for (let i = 0; i < length; i++) {
+    let randomElement = Math.floor(Math.floor(Math.random() * 10));
+    result.push(uppercase[randomElement]);
+  }
+  for (let i = 0; i < length; i++) {
+    let randomElement = Math.floor(Math.floor(Math.random() * 10));
+    result.push(specials[randomElement]);
+  }
+  let code = result.join("");
 
-  return <>Your random number is.... {integer}</>;
+  return <>Your random a code is.... {code}</>;
 }
