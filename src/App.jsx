b13715b9a1b6e81545c1c5648c46be3e8fbd16be
Cameron Blackwood <cameronjwblackwood@gmail.com>
2020-09-26 18:16:29 +0100

    functioning minus words

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import "./App.css";
 function App() {
   let [code, setCode] = useState(null);
   let [useNumbers, setUseNumbers] = useState(false);
+  let [useLowercase, setUseLowerCase] = useState(false);
+  let [useUpperCase, setUseUpperCase] = useState(false);
+  let [useSpecials, setUseSpecials] = useState(false);
+  // let [numWords, setNumWords] = useState(0);
   const handleIntegerChange = (e) => {
     setCode(e.target.value);
   };
@@ -21,18 +25,41 @@ function App() {
           />
         </li>
         <li>
-          Lowercase <input type="checkbox" />
+          Lowercase{" "}
+          <input
+            type="checkbox"
+            onChange={() => setUseLowerCase((useLowercase = !useLowercase))}
+          />
         </li>
         <li>
-          Uppercase <input type="checkbox" />{" "}
+          Uppercase{" "}
+          <input
+            type="checkbox"
+            onChange={() => setUseUpperCase((useUpperCase = !useUpperCase))}
+          />{" "}
         </li>
         <li>
-          Specials <input type="checkbox" />
+          Specials{" "}
+          <input
+            type="checkbox"
+            onChange={() => setUseSpecials((useSpecials = !useSpecials))}
+          />
         </li>
         <li>plus... how many words?</li>
+        <input type="number" />
       </ul>
       <input type="number" value={code} onChange={handleIntegerChange} />
-      {code ? randomInteger(code, 5, useNumbers) : ""}
+      {code
+        ? randomInteger(
+            code,
+            5,
+            useNumbers,
+            true,
+            useLowercase,
+            useUpperCase,
+            useSpecials
+          )
+        : ""}
     </div>
   );
 }
--- src/scripts/integer.jsx
+++ src/scripts/integer.jsx
@@ -5,45 +5,72 @@ export default function randomInteger(
   numberOfWords,
   includeNumbers,
   includeWords,
-  includeAll
+  includeLower,
+  includeUpper,
+  includeSpecial
 ) {
+  let collectFrom = [];
   const digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   const lowercase = "abcdefghijklmnopqrstuvwxyz".split("");
   const uppercase = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
   const specials = "!@£$%^&*()+_{}".split("");
-  const allChars = [digits, lowercase, uppercase, specials];
   let result = [];
-  if (includeAll) {
-    //select a random element from allChars, add a random element from within it
-  }
-  if (includeWords) {
-    let words = fetch(
-      `https://random-word-api.herokuapp.com/word?number=${numberOfWords}`
-    )
-      .then((res) => res.json())
-      .then((result) => {
-        console.log(result);
-      });
-    result.push(words);
-  }
+
   if (includeNumbers) {
+    collectFrom.push(digits);
+  }
+  if (includeLower) {
+    collectFrom.push(lowercase);
+  }
+  if (includeUpper) {
+    collectFrom.push(uppercase);
+  }
+  if (includeSpecial) {
+    collectFrom.push(specials);
+  }
+
+  if (collectFrom.length > 0) {
     for (let i = 0; i < length; i++) {
-      let randomElement = Math.floor(Math.floor(Math.random() * 10));
-      result.push(digits[randomElement]);
+      let randomElement = Math.floor(
+        Math.floor(Math.random() * collectFrom.length)
+      );
+
+      let selectedArray = collectFrom[randomElement];
+
+      result.push(
+        selectedArray[Math.floor(Math.random() * selectedArray.length)]
+      );
     }
   }
-  for (let i = 0; i < length; i++) {
-    let randomElement = Math.floor(Math.floor(Math.random() * 10));
-    result.push(lowercase[randomElement]);
-  }
-  for (let i = 0; i < length; i++) {
-    let randomElement = Math.floor(Math.floor(Math.random() * 10));
-    result.push(uppercase[randomElement]);
-  }
-  for (let i = 0; i < length; i++) {
-    let randomElement = Math.floor(Math.floor(Math.random() * 10));
-    result.push(specials[randomElement]);
-  }
+
+  // if (includeWords) {
+  //   let words = fetch(
+  //     `https://random-word-api.herokuapp.com/word?number=${numberOfWords}`
+  //   )
+  //     .then((res) => res.json())
+  //     .then((result) => {
+  //       console.log(result);
+  //     });
+  //   result.push(words);
+  // }
+  // if (includeNumbers) {
+  //   for (let i = 0; i < length; i++) {
+  //     let randomElement = Math.floor(Math.floor(Math.random() * 10));
+  //     result.push(digits[randomElement]);
+  //   }
+  // }
+  // for (let i = 0; i < length; i++) {
+  //   let randomElement = Math.floor(Math.floor(Math.random() * 10));
+  //   result.push(lowercase[randomElement]);
+  // }
+  // for (let i = 0; i < length; i++) {
+  //   let randomElement = Math.floor(Math.floor(Math.random() * 10));
+  //   result.push(uppercase[randomElement]);
+  // }
+  // for (let i = 0; i < length; i++) {
+  //   let randomElement = Math.floor(Math.floor(Math.random() * 10));
+  //   result.push(specials[randomElement]);
+  // }
   let code = result.join("");
 
   return <>Your random a code is.... {code}</>;
