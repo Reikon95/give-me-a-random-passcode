1d757f2df4eeccfe3def76089a57acba555bee45
Cameron Blackwood <cameronjwblackwood@gmail.com>
2020-10-01 14:26:11 +0100

    ts fix

--- src/App.test.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
-
-test("renders title", () => {
-  const { getByText } = render(<App />);
-  const title = getByText(/Generate a Secure Passcode/i);
-  expect(title).toBeInTheDocument();
-});
