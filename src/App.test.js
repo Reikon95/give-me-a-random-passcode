d192168fa8abe4a4cfe75c2d4800343064f44330
Cameron Blackwood <cameronjwblackwood@gmail.com>
2020-10-08 19:21:11 +0100

    tests

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,15 @@ test("renders title", () => {
   const title = getByText(/Generate a Secure Passcode/i);
   expect(title).toBeInTheDocument();
 });
+
+test("renders all checkboxes", () => {
+  const { getAllByRole } = render(<App />);
+  const checkboxes = getAllByRole("checkbox");
+  expect(checkboxes).toHaveLength(5);
+});
+
+test("all checkboxes should be unchecked", () => {
+  const { getAllByRole } = render(<App />);
+  const checkedCheckboxes = getAllByRole("checkbox", { checked: false });
+  expect(checkedCheckboxes).toHaveLength(5);
+});
